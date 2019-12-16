7c08e5ee7be2ec1f58235daca1faf541b9dd6aca
Andrew Stefanich <andrewstefanich@gmail.com>
2019-12-16 15:53:48 -0500

    gameviewmodel databinding init

--- app/src/main/java/io/astefanich/shinro/ui/game/GameFragment.kt
+++ app/src/main/java/io/astefanich/shinro/ui/game/GameFragment.kt
@@ -6,20 +6,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 
 import io.astefanich.shinro.R
+import io.astefanich.shinro.databinding.GameFragmentBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class GameFragment : Fragment() {
 
+
+    private lateinit var viewModel: GameViewModel
+    private lateinit var binding: GameFragmentBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false)
+
+        binding= DataBindingUtil.inflate(inflater,R.layout.game_fragment,container,false)
+        viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
+        binding.viewModel = viewModel
+        binding.setLifecycleOwner(this)
+        return binding.root
     }
 
 
---
+++ app/src/main/java/io/astefanich/shinro/ui/game/GameViewModel.kt
@@ -0,0 +1,17 @@
+package io.astefanich.shinro.ui.game
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GameViewModel: ViewModel() {
+
+    private val score = 10
+    private val _marblesRemaining = MutableLiveData<String>()
+    val marblesRemaining: LiveData<String>
+        get() = _marblesRemaining
+
+    init {
+        _marblesRemaining.value = "10"
+    }
+}
