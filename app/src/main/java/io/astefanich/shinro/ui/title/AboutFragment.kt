6ab13021de9e7763e9e26c8dd4cbaf42543c27ae
Andrew Stefanich <andrewstefanich@gmail.com>
2019-12-17 16:06:40 -0500

    other fragments init

---
+++ app/src/main/java/io/astefanich/shinro/ui/title/AboutFragment.kt
@@ -0,0 +1,26 @@
+package io.astefanich.shinro.ui.title
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import io.astefanich.shinro.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class AboutFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.about_fragment, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/io/astefanich/shinro/ui/title/TipsFragment.kt
@@ -0,0 +1,26 @@
+package io.astefanich.shinro.ui.title
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import io.astefanich.shinro.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class TipsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.tips_fragment, container, false)
+    }
+
+
+}
