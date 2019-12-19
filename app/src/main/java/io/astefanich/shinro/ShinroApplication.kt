9975d4be93dd330f2b16f869cb13bcad6a7ff466
Andrew Stefanich <andrewstefanich@gmail.com>
2019-12-19 12:55:20 -0500

    timber logging

--- app/src/main/java/io/astefanich/shinro/ShinroApplication.kt
+++ app/src/main/java/io/astefanich/shinro/ShinroApplication.kt
@@ -3,8 +3,15 @@ package io.astefanich.shinro
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 import io.astefanich.shinro.di.DaggerShinroAppComponent
+import timber.log.Timber
 
 class ShinroApplication : DaggerApplication() {
+
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
 
     override fun applicationInjector(): AndroidInjector<ShinroApplication> {
         return DaggerShinroAppComponent.builder().build()
