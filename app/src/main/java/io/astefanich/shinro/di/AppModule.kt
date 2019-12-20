516cfb60f0356989df4f981fb9c99f141679b662
Andrew Stefanich <andrewstefanich@gmail.com>
2019-12-19 20:32:57 -0500

    room db up an running

--- app/src/main/java/io/astefanich/shinro/di/AppModule.kt
+++ app/src/main/java/io/astefanich/shinro/di/AppModule.kt
@@ -2,23 +2,16 @@ package io.astefanich.shinro.di
 
 import android.app.Application
 import androidx.room.Room
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import io.astefanich.shinro.database.AppDatabase
 import io.astefanich.shinro.database.BoardDao
 import io.astefanich.shinro.repository.BoardRepository
-import io.astefanich.shinro.repository.FakeBoardRepository
+import io.astefanich.shinro.repository.BoardRepositoryImpl
 import javax.inject.Singleton
 
 @Module
 class AppModule {
-
-    @Singleton
-    @Provides
-    internal fun providesBoardRepository(): BoardRepository {
-        return FakeBoardRepository(Triple("EASY", "MEDIUM", "HARD"))
-    }
 
     @Singleton
     @Provides
@@ -36,39 +29,11 @@ class AppModule {
     internal fun providesBoardDao(database: AppDatabase): BoardDao {
         return database.boardDao
     }
+
+    @Singleton
+    @Provides
+    internal fun providesBoardRepository(dao: BoardDao): BoardRepository {
+        return BoardRepositoryImpl(dao)
+    }
 }
 
-//@Module
-//abstract class AppModule {
-//
-////    @Singleton
-////    @Binds
-////    internal abstract fun providesBoardRepository(repository: FakeBoardRepository): BoardRepository
-//
-//    @Module
-//    companion object {
-//
-//        @Singleton
-//        @Provides
-//        internal fun providesAppDatabase(application: Application): AppDatabase {
-//            return Room.inMemoryDatabaseBuilder(
-//                application,
-//                AppDatabase::class.java
-//            ).allowMainThreadQueries()
-//                .build()
-//
-//        }
-//
-//        @Singleton
-//        @Provides
-//        internal fun providesBoardDao(database: AppDatabase): BoardDao {
-//            return database.boardDao
-//        }
-//
-//        @Singleton
-//        @Provides
-//        internal fun providesBoardRepository(): BoardRepository {
-//            return FakeBoardRepository(Triple("EASY", "MEDIUM", "HARD"))
-//        }
-//    }
-//}
