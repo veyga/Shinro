0b8041c79eb5ee5429aa7f99c5b04cfff3253335
Andrew Stefanich <andrewstefanich@gmail.com>
2019-12-20 13:29:05 -0500

    dummy data

--- app/src/main/java/io/astefanich/shinro/di/AppModule.kt
+++ app/src/main/java/io/astefanich/shinro/di/AppModule.kt
@@ -2,6 +2,8 @@ package io.astefanich.shinro.di
 
 import android.app.Application
 import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import dagger.Module
 import dagger.Provides
 import io.astefanich.shinro.database.AppDatabase
@@ -10,20 +12,40 @@ import io.astefanich.shinro.domain.Board
 import io.astefanich.shinro.repository.BoardRepository
 import io.astefanich.shinro.repository.BoardRepositoryImpl
 import io.astefanich.shinro.repository.FakeBoardRepository
+import timber.log.Timber
 import javax.inject.Singleton
 
 @Module
 class AppModule {
 
+
     @Singleton
     @Provides
-    internal fun providesAppDatabase(application: Application): AppDatabase {
-        return Room.inMemoryDatabaseBuilder(
+    internal fun providesAppDatabase(application: Application, boards: Array<Board>): AppDatabase {
+
+        lateinit var appDatabase: AppDatabase
+        appDatabase = Room.inMemoryDatabaseBuilder(
             application,
             AppDatabase::class.java
-        ).allowMainThreadQueries()
-            .build()
+        )
+            .fallbackToDestructiveMigration()
+            .addCallback(object : RoomDatabase.Callback() {
 
+                override fun onCreate(db: SupportSQLiteDatabase) {
+                    super.onCreate(db)
+                    appDatabase.boardDao().insertBoards(*boards)
+                    Timber.i("ONCREATE") //not getting logged
+                }
+
+                override fun onOpen(db: SupportSQLiteDatabase) {
+                    super.onOpen(db)
+                    Timber.i("ONOPEN") //not getting logged
+                }
+            }
+            )
+            .build()
+        Timber.i("database is open? ${appDatabase.isOpen}") //logging false
+        return appDatabase
     }
 
     @Singleton
@@ -33,15 +55,16 @@ class AppModule {
     }
 
     @Singleton
-    @Provides
-    internal fun providesBoardRepository(boards: Array<Board>): BoardRepository {
+//    @Provides
+    internal fun providesFakeBoardRepository(boards: Array<Board>): BoardRepository {
         return FakeBoardRepository(boards)
     }
-//    @Singleton
-//    @Provides
-//    internal fun providesBoardRepository(dao: BoardDao): BoardRepository {
-//        return BoardRepositoryImpl(dao)
-//    }
+
+    @Singleton
+    @Provides
+    internal fun providesBoardRepositoryImpl(dao: BoardDao): BoardRepository {
+        return BoardRepositoryImpl(dao)
+    }
 
     @Singleton
     @Provides
