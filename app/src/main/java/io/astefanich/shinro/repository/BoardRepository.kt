ac37dfbf263abce42b33cc7af24baf2d9f44e9f8
Andrew Stefanich <andrewstefanich@gmail.com>
2020-01-03 15:12:22 -0500

    repo suspend functions

--- app/src/main/java/io/astefanich/shinro/repository/BoardRepository.kt
+++ app/src/main/java/io/astefanich/shinro/repository/BoardRepository.kt
@@ -2,17 +2,27 @@ package io.astefanich.shinro.repository
 
 import io.astefanich.shinro.database.BoardDao
 import io.astefanich.shinro.domain.Board
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 
 class BoardRepository @Inject constructor(val boardDao: BoardDao) {
 
-    fun getBoardById(boardId: Int): Board? = boardDao.getBoardById(boardId)
+    suspend fun getBoardById(boardId: Int): Board? = withContext(Dispatchers.IO) {
+        boardDao.getBoardById(boardId)
+    }
 
-    fun getLowestIncompleteBoard(): Board = boardDao.getLowestIncompleteBoard()
+    suspend fun getLowestIncompleteBoard(): Board = withContext(Dispatchers.IO) {
+        boardDao.getLowestIncompleteBoard()
+    }
 
-    fun insertBoards(vararg boards: Board) = boardDao.insertBoards(*boards)
+    suspend fun insertBoards(vararg boards: Board) = withContext(Dispatchers.IO) {
+        boardDao.insertBoards(*boards)
+    }
 
-    fun updateBoard(board: Board) = boardDao.updateBoard(board)
+    suspend fun updateBoard(board: Board) = withContext(Dispatchers.IO) {
+        boardDao.updateBoard(board)
+    }
 
 }
