4a3172232a76b0914d5af5d7626de32e7db3a4d9
Andrew Stefanich <andrewstefanich@gmail.com>
2020-08-07 22:14:03 -0400

    progress item click navigate

--- app/src/main/java/io/astefanich/shinro/util/ProgressRecyclerAdapter.kt
+++ app/src/main/java/io/astefanich/shinro/util/ProgressRecyclerAdapter.kt
@@ -3,9 +3,11 @@ package io.astefanich.shinro.util
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import io.astefanich.shinro.R
 import io.astefanich.shinro.domain.ProgressItem
+import io.astefanich.shinro.ui.ProgressFragmentDirections
 import kotlinx.android.synthetic.main.progess_item.view.*
 
 class ProgressRecyclerAdapter(private val items: List<ProgressItem>) :
@@ -35,17 +37,15 @@ class ProgressRecyclerAdapter(private val items: List<ProgressItem>) :
 
         fun bind(progressItem: ProgressItem) {
             itemView.apply {
-//                tip_image.setImageResource(progressItem.drawable)
-//                progress_item_board_id.setText("${progressItem.boardId}")
-//                progress_item_difficulty.setText(progressItem.difficulty)
-//                val icon = if (progressItem.completed) R.drawable.checkmark else R.drawable.delete
-//                progress_item_icon.setImageResource(icon)
                 progress_item_chip.setText("Puzzle ${progressItem.boardId}\t(${progressItem.difficulty})      ")
                 val icon = if (progressItem.completed) R.drawable.checkmark else R.drawable.delete
                 progress_item_chip.setChipIconResource(icon)
+                progress_item_chip.setOnClickListener {
+                    findNavController().navigate(
+                        ProgressFragmentDirections.actionProgressToGame(progressItem.boardId)
+                    )
+                }
             }
         }
     }
-
-
 }
