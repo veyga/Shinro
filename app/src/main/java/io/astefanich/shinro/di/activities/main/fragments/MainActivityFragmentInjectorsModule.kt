ebba3570d9c8952380c51a8bb542230f319c8057
Andrew Stefanich <andrewstefanich@gmail.com>
2020-08-19 11:52:01 -0400

    dagger activity layer almost

--- app/src/main/java/io/astefanich/shinro/di/activities/main/fragments/MainActivityFragmentInjectorsModule.kt
+++ app/src/main/java/io/astefanich/shinro/di/activities/main/fragments/MainActivityFragmentInjectorsModule.kt
@@ -7,13 +7,14 @@ import io.astefanich.shinro.di.activities.main.fragments.about.AboutModule
 import io.astefanich.shinro.ui.AboutFragment
 import io.astefanich.shinro.ui.TitleFragment
 
+//@PerFragment
 @Module
 abstract class MainActivityFragmentInjectorsModule {
 
     @ContributesAndroidInjector
     abstract fun providesTitleFragment(): TitleFragment
 
-    @PerFragment
+    @PerFragment //scope goes here, not above @Module
     @ContributesAndroidInjector(modules = [AboutModule::class])
     abstract fun providesAboutFragment(): AboutFragment
 
--- app/src/main/java/io/astefanich/shinro/di/activities/main/fragments/about/AboutComponent.kt
+++ app/src/main/java/io/astefanich/shinro/di/activities/main/fragments/about/AboutComponent.kt
@@ -1,11 +1,13 @@
 package io.astefanich.shinro.di.activities.main.fragments.about
 
 import dagger.Subcomponent
+import dagger.android.AndroidInjector
 import io.astefanich.shinro.di.PerFragment
+import io.astefanich.shinro.ui.AboutFragment
 
-@PerFragment
-@Subcomponent(modules = [AboutModule::class])
-interface AboutComponent {
-
-}
+//@PerFragment
+//@Subcomponent(modules = [AboutModule::class])
+//interface AboutComponent {//: AndroidInjector<AboutFragment>{
+//
+//}
 
